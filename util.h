7ed1e506d5cae7c24ac29e5d2caaf5525f94c70a
pnosker <pnosker@gmail.com>
2014-05-21 01:19:32 -0400

    Edited for VeriCoin

--- util.h
+++ util.h
@@ -255,8 +255,8 @@
     void hash160ToAddr(
               uint8_t *addr,
         const uint8_t *hash160,
-        #if defined(LITECOIN)
-              uint8_t type = 48
+        #if defined(VERICOIN)
+              uint8_t type = 70
         #else
               uint8_t type = 0
         #endif
